63f51de9187596c6ae93d2c73f8a9b374b0772a5
Slawomir Borowiec <przodownik@tlen.pl>
2015-07-25 23:25:07 +0200

    javaConfig conditional flow added

---
+++ src/main/java/pl/java/scalatech/job/ConditionalJob.java
@@ -0,0 +1,77 @@
+package pl.java.scalatech.job;
+
+import javax.annotation.PostConstruct;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+@Configuration
+@Slf4j
+@Profile("conditional")
+public class ConditionalJob {
+
+    @PostConstruct
+    public void init() {
+        log.info("+++ conditionalJob");
+    }
+    
+    @Autowired
+    JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job cjob(){
+        return jobBuilderFactory
+                .get("job")
+                .start(stepFirst()).on("FAILED").to(stepSecond())
+                .from(stepSecond()).on("COMPLETED").to(stepThird())
+                .from(stepThird()).on("COMPLETED").to(stepThird())
+                .end()
+                .build();
+    }
+
+    Step stepFirst(){
+        return stepBuilderFactory
+                .get("stepFirst")
+                .tasklet((stepContribution, chunkContext) -> {
+                        log.info("+++  1 step executed");
+                        stepContribution.setExitStatus(ExitStatus.FAILED);
+                        return RepeatStatus.FINISHED;
+
+                })
+                .build();
+    }
+
+    Step stepSecond(){
+        return stepBuilderFactory
+                .get("stepSecond")
+                .tasklet((stepContribution, chunkContext) ->
+                {
+                    log.info("+++  2 step executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    Step stepThird(){
+        return stepBuilderFactory
+                .get("step3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    log.info("+++ 3 step executed ");
+                    return null;
+                })
+                .build();
+    }
+
+}
---
+++ src/test/java/springBatchBootSimple/ConditionalJavaConfigJobTest.java
@@ -0,0 +1,69 @@
+package springBatchBootSimple;
+
+import java.util.Date;
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import pl.java.scalatech.config.BatchConfig;
+import pl.java.scalatech.job.ConditionalJob;
+
+import com.google.common.collect.Maps;
+
+
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes= {BatchConfig.class,ConditionalJob.class})
+@ActiveProfiles("conditional")
+public class ConditionalJavaConfigJobTest {
+
+    @Autowired
+    private Job cjob;
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private JobExplorer jobExplorer;
+    
+    @Test
+    public void shouldInterfacesInstanceExists() {
+        Assertions.assertThat(jobRepository).isNotNull();
+        Assertions.assertThat(jobExplorer).isNotNull();
+    }
+    
+    @Test
+   
+    public void shouldTaskletWork() throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
+        Map<String,JobParameter> params = Maps.newHashMap();
+        params.put("time", new JobParameter(new Date()));
+        JobExecution execution = jobLauncher.run(cjob, new JobParameters(params));
+        log.info("Exit Status :  {}", execution.getExitStatus());
+        Assert.assertEquals(ExitStatus.COMPLETED, execution.getExitStatus());
+           
+    }
+    
+    
+}
